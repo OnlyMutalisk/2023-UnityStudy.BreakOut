f2884df1e337f874946974eae95959642609d192
Onlymutalisk <onlymutalisk@gmail.com>
2023-05-11 03:53:29 +0900

    Step2: Refer to animations & scripts

---
+++ Assets/Paddle.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Paddle : MonoBehaviour
+{
+    public string[] stageStr;
+    public Sprite[] B;
+    public GameObject P_Item;
+    public SpriteRenderer P_ItemSr;
+    public Text StageText;
+    public Text ScoreText;
+    public GameObject Life0;
+    public GameObject Life1;
+    public GameObject WinPanel;
+    public GameObject GameOverPanel;
+    public GameObject PausePanel;
+    public AudioSource S_Break;
+    public AudioSource S_Eat;
+    public AudioSource S_Fail;
+    public AudioSource S_Gun;
+    public AudioSource S_HardBreak;
+    public AudioSource S_Paddle;
+    public AudioSource S_Victory;
+    public Transform ItemsTr;
+    public Transform BlocksTr;
+    public BoxCollider2D[] BlockCol;
+    public GameObject[] Ball;
+    public Animator[] BallAni;
+    public Transform[] BallTr;
+    public SpriteRenderer[] BallSr;
+    public Rigidbody2D[] BallRg;
+    public GameObject[] Bullet;
+    public SpriteRenderer PaddleSr;
+    public BoxCollider2D PaddleCol;
+    public GameObject Magnet;
+    public GameObject Gun;
+
+    bool isStart;
+    public float paddleX;
+    public float ballSpeed;
+    float oldBallSpeed = 300;
+    float paddleBorder = 2.262f;
+    float paddleSize = 1.58f;
+    int combo;
+    int score;
+    int stage;
+
+#if (UNITY_ANDROID)
+    void Awake()
+    {
+        Screen.SetResolution(1080, 1920, false);
+    }
+#else
+    void Awake()
+    {
+        Screen.SetResolution(540, 960, false);
+    }
+#endif
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(PausePanel.activeSelf)
+            {
+                PausePanel.SetActive(false);
+                Time.timeScale = 1;
+            }
+            else
+            {
+                PausePanel.SetActive(true);
+                Time.timeScale = 0;
+            }
+        }
+    }
+}
